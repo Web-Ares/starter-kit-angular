cd0dc95b4e57e724ee45f8b888b6f7782feb06fa
aleksandrnikolajcuk <deathstars111>
2017-01-07 10:57:18 +0200

    First

---
+++ app/ts/components/pages/contact.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'contact',
+    template: 'Contact Component text',
+    styleUrls: [ 'app/css/app.component.css' ]
+})
+export class ContactComponent { }
--- app/ts/modules/app.module.ts
+++ app/ts/modules/app.module.ts
@@ -4,14 +4,26 @@ import { AppComponent }  from '../components/app.component';
 import { WeatherItemComponent }  from '../components/weather/weather-item.component';
 import { WeatherListComponent }  from '../components/weather/weather-list.component';
 import { WeatherService }  from '../components/weather/weather.service';
+import { ContactComponent }  from '../components/pages/contact.component';
 import {WeatherSearchComponent} from "../components/weather/weather-search.component";
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import {ContactComponent} from '../components/pages/contact.component';
 
 
 @NgModule({
-    imports:      [ BrowserModule,FormsModule,HttpModule ],
-    declarations: [ AppComponent, WeatherItemComponent, WeatherListComponent, WeatherSearchComponent ],
+    imports:      [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {
+                path: 'contact',
+                component: ContactComponent
+            }
+        ])],
+    declarations: [ AppComponent, WeatherItemComponent, WeatherListComponent, WeatherSearchComponent,ContactComponent ],
     bootstrap:    [ AppComponent ],
     providers:    [WeatherService]
 })
