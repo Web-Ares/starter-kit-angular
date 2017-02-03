5b21cfae809c45ae182e1b3be0ca73f6fcfbe7f6
Nikolas Petrunin <petruninnikolay@gmail.com>
2017-02-03 22:50:47 +0200

    test

--- app/ts/components/app.component.ts
+++ app/ts/components/app.component.ts
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-    public data = {}
+    public data:Object = {};
+    
+    constructor(){
+        this.login();
+    }
+
+    public login(){
+        let token = window.localStorage.getItem( 'token' );
+
+        if( token ){
+            this.data[ 'token' ] = token;
+        } else {
+            this.checkCode();
+        }
+
+    }
+
+    public checkCode(){
+        let search = location.search.substr( 1 ).split( '&' ),
+            token:string = null;
+        
+        search.forEach( function ( item:string ) {
+
+            let str = item.split( '=' );
+
+            if( str[ 0 ] == 'code' ){
+                token = str[ 1 ];
+
+                return false;
+            }
+
+        } );
+
+        if( !token ){
+            window.localStorage.setItem( 'token', token );
+
+            this.data[ 'token' ] = token;
+        }
+    }
 
 }
+
